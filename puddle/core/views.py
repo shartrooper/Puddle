a834bf9432e2b912e689504974f759a5ab4a72f4
Marko <michinaux.marko@gmail.com>
2023-08-05 21:15:16 -0400

    add items and categories in frontpage

--- puddle/core/views.py
+++ puddle/core/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
-
+from item.models import Category, Item
 # Create your views here.
 
 def index(req):
-   return render(req, 'core/index.html')
+    items = Item.objects.filter(is_sold=False)[0:6]
+    categories = Category.objects.all()
+    return render(req, 'core/index.html', {
+        'categories': categories,
+        'items': items,
+    })
 
 def contact(req):
-   return render(req, 'core/contact.html')
+    return render(req, 'core/contact.html')
--- puddle/puddle/settings.py
+++ puddle/puddle/settings.py
@@ -15,8 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-print(BASE_DIR)
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
@@ -38,7 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'core'
+    'core',
+    'item'
 ]
 
 MIDDLEWARE = [
@@ -118,6 +117,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+MEDIA_URL = 'media/'
+
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
